a02f6de1d4060d57f610a7127ef49f121d283062
KIMMKyungsik <93589990+KIMMKyungsik@users.noreply.github.com>
2021-11-17 17:53:27 +0900

    서버가 주는 code Int값 추출 - 로그인 성공/실패 판단 근거로 사용 - 토스트 (UI 조작) -> runOnUiThread {} 안에서 실행

--- app/src/main/java/org/techtown/colosseum/MainActivity.kt
+++ app/src/main/java/org/techtown/colosseum/MainActivity.kt
@@ -3,11 +3,12 @@ package org.techtown.colosseum
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import org.json.JSONObject
 import org.techtown.colosseum.databinding.ActivityMainBinding
 
-class MainActivity :BaseActivity() {
+class MainActivity : BaseActivity() {
 
     lateinit var binding: ActivityMainBinding
 
@@ -22,41 +23,57 @@ class MainActivity :BaseActivity() {
     }
 
 
-
-
     override fun setupEvents() {
 
-        binding.btnLogin.setOnClickListener{
+        binding.btnLogin.setOnClickListener {
 
             val inputEmail = binding.edtEmail.text.toString()
             val inputPw = binding.edtPassword.text.toString()
 
 //            서버에서 이메일 / 비번이 맞는 계정인지? 로그인 요청
 
-            ServerUtil.postRequestLogin(inputEmail, inputPw, object : ServerUtil.JsonResponseHandler {
+            ServerUtil.postRequestLogin(
+                inputEmail,
+                inputPw,
+                object : ServerUtil.JsonResponseHandler {
 
-                override fun onResponse(jsonObj: JSONObject) {
+                    override fun onResponse(jsonObj: JSONObject) {
 //                    로그인 API를 호출하고 돌아온 상황
 //                    결과로 jsonObj 하나를 받아서 돌아온 상황
 
-                    Log.d("화면에서의 jsonObj",jsonObj.toString())
+                        Log.d("화면에서의 jsonObj", jsonObj.toString())
+
+                        val code = jsonObj.getInt("code")
+
+//                    code : 200 -> 로그인 성공 토스트
+
+//                    그 외 -> 로그인 실패 토스트
+
+
+                        runOnUiThread {
+
+                            if (code == 200) {
+                                Toast.makeText(mContext, "로그인 성공", Toast.LENGTH_SHORT).show()
+
+                            }
+                            else {
+                                Toast.makeText(mContext, "로그인 실패", Toast.LENGTH_SHORT).show()
+
+                                }
+                            }
+                            }
+
+                        })
+
+
+                    }
+
+
+                }
+
+                        override fun setValues() {
+
                 }
 
 
-
-            })
-
-
-
         }
-
-
-
-    }
-
-    override fun setValues() {
-
-    }
-
-
-}

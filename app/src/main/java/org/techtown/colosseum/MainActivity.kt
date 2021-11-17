15b504de7d4203c61a63a9f69ca8b403b4438288
KIMMKyungsik <93589990+KIMMKyungsik@users.noreply.github.com>
2021-11-17 14:56:39 +0900

    UI작업

--- app/src/main/java/org/techtown/colosseum/MainActivity.kt
+++ app/src/main/java/org/techtown/colosseum/MainActivity.kt
@@ -12,5 +12,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+
+
+
     }
 }
